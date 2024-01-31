06cd87c66699d24d64570c3b79b79ff2ee48dd4d
Paul Rey <contact@paulrey.io>
2024-01-31 17:11:22 +0100

    Update

--- .config/waybar/scripts/album_art.sh
+++ .config/waybar/scripts/album_art.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-album_art=$(playerctl -p spotify metadata mpris:artUrl)
-album_name_and_artist="$(playerctl -p spotify metadata xesam:album)-$(playerctl -p spotify metadata xesam:artist)"
+
+album_art=$(playerctl -p spotify metadata mpris:artUrl 2> /dev/null)
+album_name_and_artist=$(playerctl -p spotify metadata xesam:album 2> /dev/null)-$(playerctl -p spotify metadata xesam:artist 2> /dev/null)
 
 mkdir -p /tmp/covers
 
-if [[ -z $album_art ]]; then
-   # spotify is dead, we should die too.
-   echo ""
-   exit 1
+if [ "${album_art}" = "" ] || [[ -z $album_art ]]; then
+   echo "/tmp/covers/empty"
+   exit 0
 fi
 
 path="/tmp/covers/${album_name_and_artist}.jpeg"
 
 if [ -f "${path}" ]; then
     echo "${path}"
-    echo "<img width=\"126\" src\"${path}\"/>"
     exit 0
 fi
 
 curl -s  "${album_art}" --output "${path}"
 echo "${path}"
-echo "<img width=\"126\" src=\"${path}\"/>"
